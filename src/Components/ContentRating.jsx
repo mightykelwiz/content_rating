a4d7b711863f3b101a30d4c225653cd0b9e9fb60
MightKelwiz <kevin.kedumele.kk@gmail.com>
2026-01-09 08:59:34 -0500

    updated

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -1,4 +1,3 @@
-
 import React, { Component } from 'react';
 import './ContentRating.css';
 
@@ -8,41 +7,38 @@ class ContentRating extends Component {
     this.state = {
       likes: 0,
       dislikes: 0,
-
-        handleLike: () => {
+      totalRatings: 0,
+      handleLike: () => {
         this.setState((prevState) => ({
-            likes: prevState.likes + 1,
-            totalRatings: prevState.totalRatings + 1
-            }));
-        },
-        handleDislike: () => {
-            this.setState((prevState) => ({
-            dislikes: prevState.dislikes + 1,
-            totalRatings: prevState.totalRatings + 1
-            }));
-        }
+          likes: prevState.likes + 1,
+          totalRatings: prevState.totalRatings + 1
+        }));
+      },
+      handleDislike: () => {
+        this.setState((prevState) => ({
+          dislikes: prevState.dislikes + 1,
+          totalRatings: prevState.totalRatings + 1
+        }));
+      }
     };
   }
   render() {
     return (
-     <>
-     <div className='content-rating'>
-        <p>
-
-        </p>
-        <div className='rating-buttons'>
-        <button className="like-button" onClick={this.state.handleLike}>
-            Like ({this.state.likes})
-        </button>
-        <button className="dislike-button" onClick={this.state.handleDislike}>
-            Dislike ({this.state.dislikes})
-        </button>
-            <p>Total Ratings: {this.state.totalRatings}</p>
+      <>
+        <h1>Content Rating</h1>
+        <div className='content-rating'>
+          <div className='rating-buttons'>
+            <button className="like-button" onClick={this.state.handleLike}>
+              Like ({this.state.likes})
+            </button>
+            <button className="dislike-button" onClick={this.state.handleDislike}>
+              Dislike ({this.state.dislikes})
+            </button>
+          </div>
+          <p>Total Ratings: {this.state.totalRatings}</p>
         </div>
-     </div>
-     </>
+      </>
     );
   }
 }
-
 export default ContentRating;
